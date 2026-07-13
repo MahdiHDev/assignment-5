b2a4cbe1087e9e775ba7b0347fcb54e303e9a34a
MahdiHDev <mahdi.hussain.work@gmail.com>
2026-07-13 18:26:41 +0600

    fix: update server file references from .js to .cjs for compatibility with Vercel

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import app from "./app";
 import { prisma } from "./lib/prisma";
 
 const bootstrap = async () => {
-  try {
-    await prisma.$connect();
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on http://localhost:${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    await prisma.$disconnect();
-    process.exit(1);
-  }
+    try {
+        await prisma.$connect();
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(
+                `Server is running on http://localhost:${process.env.PORT}`,
+            );
+        });
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
 };
 
 bootstrap();
